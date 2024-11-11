dc41f21b16f3cec11edf913fbb98f9b06d7c98d4
Parker Moore <237985+parkr@users.noreply.github.com>
2024-11-11 15:40:27 -0800

    Create m4b-tool.rb
    Key addition here is to add --install-from-source support

---
+++ Formula/m4b-tool.rb
@@ -0,0 +1,35 @@
+# brew tap parkr/tap
+# brew install m4b-tool
+class M4bTool < Formula
+  version "pre-release"
+  desc "m4b-tool is a command line utility to merge, split and chapterize audiobook files such as mp3, ogg, flac, m4a or m4b"
+  homepage "https://github.com/sandreas/m4b-tool"
+  url "https://github.com/sandreas/m4b-tool/files/10728378/m4b-tool.tar.gz"
+  sha256 "181fb3e332849ea704ef860aaabaf9dcab2fdab298ffba99e66d31efa18b02f2"
+  license "MIT"
+
+  head do
+    url "https://github.com/sandreas/m4b-tool.git", branch: "master"
+
+    depends_on "composer" => :build
+  end
+
+  depends_on "php"
+  depends_on "sandreas/tap/mp4v2"
+  depends_on "fdk-aac-encoder"
+  # depends_on "homebrew-ffmpeg/ffmpeg/ffmpeg" => 'with-fdk-aac'
+
+  def install
+    if head?
+      system "./build"
+      mv "dist/m4b-tool.phar" "m4b-tool.phar"
+    end
+    
+    bin.install "m4b-tool.phar" => "m4b-tool"
+  end
+
+  test do
+    assert_equal "m4b-tool v.#{version}", shell_output("#{bin}/m4b-tool --version").chomp
+  end
+
+end
